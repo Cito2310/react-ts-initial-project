fcd10777f49bb33e94140a2addafd8653761eeee
Cito2310 <cito2310@gmail.com>
2024-11-08 19:40:07 -0300

    update dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { ShowNumber } from "./ShowNumber"
-
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-[#1F1F1F]">
-        <ShowNumber/>
+    <div className="App">
+      Hello World
     </div>
   )
 }
