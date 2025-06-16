0b89e48a809dedaf233e3018d172982c938fc436
Cito2310 <cito2310@gmail.com>
2025-06-16 18:07:25 -0300

    added category-form-dropdown

--- src/CategoryManagement/components/ListItemCategory.tsx
+++ src/CategoryManagement/components/ListItemCategory.tsx
@@ -8,9 +8,9 @@ interface props {
 
 
 export const ListItemCategory = ({ section, name }: props) => {
-    const [height, setHeight] = useState(0)
+    const [height, setHeight] = useState<number | string>(0)
     const toggleDetailsMenu = () => {
-        if ( height === 0 ) setHeight(300);
+        if ( height === 0 ) setHeight("auto");
         if ( height !== 0 ) setHeight(0);
     }
 
@@ -32,12 +32,108 @@ export const ListItemCategory = ({ section, name }: props) => {
             <td colSpan={10}>
                 <div style={{height}} className={`
                     ${height !== 0 ? "px-6 py-5" : ""}
-                    bg-white transition-base overflow-hidden flex flex-col shadow-md shadow-[#8f8f8f] rounded-b-md `}>
-                    <input value={name}></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
-                    <input></input>
+                    bg-white transition-base overflow-hidden flex flex-col shadow-md shadow-[#8f8f8f] rounded-b-md gap-4`}>
+
+
+                        <div className="flex gap-4 items-center justify-between">
+                            <div className="flex gap-4">
+                                <div className="flex flex-col">
+                                    <label className="text-sm font-medium px-1">Nombre de la categoria</label>
+                                    <input onChange={()=>{}} className="border-b-2 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Aceite"}/>
+                                </div>
+
+                                <div className="flex flex-col">
+                                    <label className="text-sm font-medium px-1">Sección</label>
+                                    <select defaultValue={"g"} className="h-full border-b-2 border-[#d5e0e0] py-[6px] outline-none focus:border-[#7e9292] transition-base">
+                                        <option value={"g"}>Alimentos</option>
+                                        <option value={"kg"}>Higiene Personal</option>
+                                        <option value={"l"}>Bebidas</option>
+                                        <option value={"u"}>Verduleria</option>
+                                        <option value={"u"}>Carniceria</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <button className="aspect-square rounded-md px-3 active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] flex gap-1 justify-center items-center shadow-md text-lg">
+                                <i className="fa-solid fa-plus"></i>
+                            </button>
+                        </div>
+
+
+
+                        {/* CONTIENE LOS CONTAINES DE SUBCATEGORIAS */}
+                        <div className="flex gap-4 w-full flex-wrap">
+                            <div className="flex flex-col flex-1 min-w-[500px] gap-4">
+                                 <div className="flex gap-4">
+                                    <input onChange={()=>{}} className="border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Aceite de Girasol"}/>
+
+                                    <button className="aspect-square rounded-md px-2 active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] flex gap-1 justify-center items-center shadow-md text-lg">
+                                    <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                    <button className="aspect-square rounded-md px-2 active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] flex gap-1 justify-center items-center shadow-md text-lg">
+                                    <i className="fa-solid fa-plus"></i>
+                                    </button>
+                                </div> 
+
+                                <div className="h-auto flex flex-wrap gap-x-4 gap-y-2">
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col flex-1 min-w-[500px] gap-4">
+                                 <div className="flex gap-4">
+                                    <input onChange={()=>{}} className="border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Aceite de Girasol"}/>
+
+                                    <button className="aspect-square rounded-md px-2 active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] flex gap-1 justify-center items-center shadow-md text-lg">
+                                    <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                    <button className="aspect-square rounded-md px-2 active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] flex gap-1 justify-center items-center shadow-md text-lg">
+                                    <i className="fa-solid fa-plus"></i>
+                                    </button>
+                                </div> 
+
+                                <div className="h-auto flex flex-wrap gap-x-4 gap-y-2">
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                    <div className="flex flex-row [width:calc(50%-0.5rem)] gap-4">
+                                        <input onChange={()=>{}} className="w-full border-b-2 flex-1 border-[#d5e0e0] px-1 py-1 outline-none focus:border-[#7e9292] transition-base" value={"Cañuelas"}/>
+                                        <button className="aspect-square rounded-md active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff]  border-[#7e9292] text-[#7e9292] flex justify-center items-center shadow-md text-sm w-9"><i className="fa-solid fa-trash"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+
+                        <div className="flex gap-4 flex-wrap justify-end">
+                            <button className="w-[200px] active:brightness-[.90] hover:brightness-95 transition-base font-medium bg-[#ffffff] border-2 border-[#7e9292] text-[#7e9292] rounded-md py-1.5 px-4  shadow-[#747474] flex gap-1 justify-center items-center">
+                                Cancelar
+                            </button>
+                            <button className="w-[200px] active:brightness-[.90] hover:brightness-95 transition-base bg-[#DD656F] border border-[#D9545F] text-[#fff] rounded-md py-1.5 px-4  shadow-[#747474] flex gap-1 justify-center items-center">
+                                Eliminar Categoria
+                            </button>
+                            <button className="w-[200px] active:brightness-[.90] hover:brightness-95 transition-base bg-[#008080] border border-[#007070] text-[#fff] rounded-md py-1.5 px-4 flex gap-1 justify-center items-center">
+                                Editar Categoria
+                            </button>
+                        </div>
                 </div>
             </td>
         </tr>
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { CategoryManagement } from './CategoryManagement/Main'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ProductManagement />
-    {/* <CategoryManagement /> */}
+    {/* <ProductManagement /> */}
+    <CategoryManagement />
   </React.StrictMode>,
 )
